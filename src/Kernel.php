71e2972fa5181922ed6774182aabed8162ee49a9
Marcelo Saldanha <saldanha@uttara.com.br>
2025-04-01 12:54:38 -0300

    Preparação para uma miniaplicação para uso no desenvolvimento do plugin

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Saldanhakun\SecureApplication;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
